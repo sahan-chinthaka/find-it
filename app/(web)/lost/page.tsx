344904553ac9b3003d20dc680827bbc11c9df0ed
sahan-chinthaka <sahanchinthaka04@gmail.com>
2024-03-01 20:41:04 +0530

    View all lost items in lost page

--- app/(web)/lost/page.tsx
+++ app/(web)/lost/page.tsx
@@ -1,5 +1,6 @@
 import { cookiesToString } from "@/lib/utils";
 import { cookies } from "next/headers";
+import Link from "next/link";
 
 async function LostPage() {
 	const c = cookies();
@@ -10,7 +11,18 @@ async function LostPage() {
 	});
 	const data = await losts.json();
 
-	return JSON.stringify(data);
+	return (
+		<div>
+			<Link className="text-blue-500" href={"/new-lost"}>New lost</Link>
+			{data.map((a: any) => (
+				<div className="p-2 m-2 bg-slate-200">
+					<p>{a.title}</p>
+					<p>{a.description}</p>
+					<p><Link className="text-blue-500" href={"/lost/" + a.id}>Details</Link></p>
+				</div>
+			))}
+		</div>
+	);
 }
 
 export default LostPage;
