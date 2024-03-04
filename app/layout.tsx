66f33979acd1145a269fc16d5c87df4b48fee2a9
sahan-chinthaka <sahanchinthaka04@gmail.com>
2024-03-04 16:22:43 +0530

    Change map loading

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,6 @@ export default function RootLayout({
 		<html lang="en">
 			<script
 				async
-				defer
 				src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAP_API}&libraries=places&loading=async`}
 			></script>
 			<body>
