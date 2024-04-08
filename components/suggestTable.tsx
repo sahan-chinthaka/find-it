80d7e1f0ef92d06ede699eef3afdb6f217018d7a
Randika Dilshan <rdilshan077788@gmail.com>
2024-04-08 19:03:10 +0530

    add gif for suggest table

--- components/suggestTable.tsx
+++ components/suggestTable.tsx
@@ -52,31 +52,55 @@ export default function suggestTable() {
 
   return (
     <ul className="divide-y">
-      {suggestItems.map((item) => (
-        <li className="flex items-center justify-between p-4 hover:bg-gray-100">
-          <Link href={`suggestion/${item.id}`}>
-            <div className="flex items-center gap-4">
-              <img
-                alt="Avatar"
-                className="rounded-full"
-                height="40"
-                src="https://github.com/shadcn.png"
-                style={{
-                  aspectRatio: "40/40",
-                  objectFit: "cover",
-                }}
-                width="40"
-              />
-              <div className="grid gap-1.5">
-                <h3 className="font-semibold">{item.name}</h3>
-                <p className="text-sm text-gray-500 dark:text-gray-400">
-                  {item.description}
-                </p>
-              </div>
+      {suggestItems.length === 0 ? (
+        <section className="flex items-start  py-6">
+          <div className="container flex flex-col items-center px-4 space-y-4">
+            <div className="flex flex-col items-center space-y-2 text-center">
+              <p className="text-lg font-medium text-gray-500 dark:text-gray-400">
+                No have suggestion item
+              </p>
             </div>
-          </Link>
-        </li>
-      ))}
+            
+            <img
+              alt="GIF"
+              className="aspect-square"
+              height="150"
+              src="https://i.postimg.cc/B6yVSWfW/searching-find.gif"
+              width="150"
+            />
+            
+          </div>
+        </section>
+      ) : (
+        suggestItems.map((item) => (
+          <li
+            className="flex items-center justify-between p-4 hover:bg-gray-100"
+            key={item.id}
+          >
+            <Link href={`suggestion/${item.id}`}>
+              <div className="flex items-center gap-4">
+                <img
+                  alt="Avatar"
+                  className="rounded-full"
+                  height="40"
+                  src="https://github.com/shadcn.png"
+                  style={{
+                    aspectRatio: "40/40",
+                    objectFit: "cover",
+                  }}
+                  width="40"
+                />
+                <div className="grid gap-1.5">
+                  <h3 className="font-semibold">{item.name}</h3>
+                  <p className="text-sm text-gray-500 dark:text-gray-400">
+                    {item.description}
+                  </p>
+                </div>
+              </div>
+            </Link>
+          </li>
+        ))
+      )}
     </ul>
   );
 }
