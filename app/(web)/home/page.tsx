68803e0e9bbd69799f138d4ae8df0802ebcef93c
Randika Dilshan <rdilshan077788@gmail.com>
2024-04-09 16:27:42 +0530

    suggest Table add

--- app/(web)/home/page.tsx
+++ app/(web)/home/page.tsx
@@ -1,179 +1,260 @@
-"use client";
+
 
 import * as React from "react";
-import Link from "next/link";
-import { Menu, Search } from "lucide-react";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import {
-	DropdownMenu,
-	DropdownMenuContent,
-	DropdownMenuItem,
-	DropdownMenuLabel,
-	DropdownMenuSeparator,
-	DropdownMenuTrigger,
-  } from "@/components/ui/dropdown-menu"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
-  
-import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Tabs, TabsContent } from "@/components/ui/tabs";
+import Link from "next/link";
+import SuggestTable from "@/components/suggestTable";
 
 export default function HomePage() {
-  const [state, setState] = React.useState(false);
-
   return (
-	<div>
-
-
-	<div>
-	<ScrollArea className="h-full">
-      <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
-        <div className="flex items-center justify-between space-y-2">
-          <h2 className="text-3xl font-bold tracking-tight">
-            Hi, Welcome back 👋
-          </h2>
-          
-        </div>
-        <Tabs defaultValue="overview" className="space-y-4">
-         
-          <TabsContent value="overview" className="space-y-4">
-            <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-              <Card>
-                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                  <CardTitle className="text-sm font-medium">
-                    Total Revenue
-                  </CardTitle>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="h-4 w-4 text-muted-foreground"
-                  >
-                    <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-                  </svg>
-                </CardHeader>
-                <CardContent>
-                  <div className="text-2xl font-bold">$45,231.89</div>
-                  <p className="text-xs text-muted-foreground">
-                    +20.1% from last month
-                  </p>
-                </CardContent>
-              </Card>
-              <Card>
-                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                  <CardTitle className="text-sm font-medium">
-                    Subscriptions
-                  </CardTitle>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="h-4 w-4 text-muted-foreground"
-                  >
-                    <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-                    <circle cx="9" cy="7" r="4" />
-                    <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
-                  </svg>
-                </CardHeader>
-                <CardContent>
-                  <div className="text-2xl font-bold">+2350</div>
-                  <p className="text-xs text-muted-foreground">
-                    +180.1% from last month
-                  </p>
-                </CardContent>
-              </Card>
-              <Card>
-                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                  <CardTitle className="text-sm font-medium">Sales</CardTitle>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="h-4 w-4 text-muted-foreground"
-                  >
-                    <rect width="20" height="14" x="2" y="5" rx="2" />
-                    <path d="M2 10h20" />
-                  </svg>
-                </CardHeader>
-                <CardContent>
-                  <div className="text-2xl font-bold">+12,234</div>
-                  <p className="text-xs text-muted-foreground">
-                    +19% from last month
-                  </p>
-                </CardContent>
-              </Card>
-              <Card>
-                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                  <CardTitle className="text-sm font-medium">
-                    Active Now
-                  </CardTitle>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    className="h-4 w-4 text-muted-foreground"
-                  >
-                    <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-                  </svg>
-                </CardHeader>
-                <CardContent>
-                  <div className="text-2xl font-bold">+573</div>
-                  <p className="text-xs text-muted-foreground">
-                    +201 since last hour
-                  </p>
-                </CardContent>
-              </Card>
+    <div>
+      <div>
+        <ScrollArea className="h-full">
+          <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
+            <div className="flex items-center justify-between space-y-2">
+              <h2 className="text-3xl font-bold tracking-tight">
+                Hi, Welcome back 👋
+              </h2>
             </div>
-            <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-7">
-              <Card className="col-span-4">
-                <CardHeader>
-                  <CardTitle>Overview</CardTitle>
-                </CardHeader>
-                <CardContent className="pl-2">
-                 
-                </CardContent>
-              </Card>
-              <Card className="col-span-4 md:col-span-3">
-                <CardHeader>
-                  <CardTitle>Recent Sales</CardTitle>
-                  <CardDescription>
-                    You made 265 sales this month.
-                  </CardDescription>
-                </CardHeader>
-                <CardContent>
+            <Tabs defaultValue="overview" className="space-y-4">
+              <TabsContent value="overview" className="space-y-4">
+                <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
+                  <Card>
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                      <CardTitle className="text-sm font-medium">
+                        Total lost item
+                      </CardTitle>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        className="h-4 w-4 text-muted-foreground"
+                      >
+                        <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
+                      </svg>
+                    </CardHeader>
+                    <CardContent>
+                      <div className="text-2xl font-bold">8</div>
+                      <p className="text-xs text-muted-foreground">
+                        
+                      </p>
+                    </CardContent>
+                  </Card>
+                  <Card>
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                      <CardTitle className="text-sm font-medium">
+                        Total Found Item
+                      </CardTitle>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        className="h-4 w-4 text-muted-foreground"
+                      >
+                        <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
+                        <circle cx="9" cy="7" r="4" />
+                        <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
+                      </svg>
+                    </CardHeader>
+                    <CardContent>
+                      <div className="text-2xl font-bold">0</div>
+                      <p className="text-xs text-muted-foreground">
+                        
+                      </p>
+                    </CardContent>
+                  </Card>
+                  <Card>
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                      <CardTitle className="text-sm font-medium">
+                        Total Suggestion Items
+                      </CardTitle>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        className="h-4 w-4 text-muted-foreground"
+                      >
+                        <rect width="20" height="14" x="2" y="5" rx="2" />
+                        <path d="M2 10h20" />
+                      </svg>
+                    </CardHeader>
+                    <CardContent>
+                      <div className="text-2xl font-bold">2</div>
+                      <p className="text-xs text-muted-foreground">
+                       
+                      </p>
+                    </CardContent>
+                  </Card>
+                  <Card>
+                    <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                      <CardTitle className="text-sm font-medium">
+                        Finish Item
+                      </CardTitle>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        className="h-4 w-4 text-muted-foreground"
+                      >
+                        <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
+                      </svg>
+                    </CardHeader>
+                    <CardContent>
+                      <div className="text-2xl font-bold">0</div>
+                      <p className="text-xs text-muted-foreground">
+                      
+                      </p>
+                    </CardContent>
+                  </Card>
+                </div>
 
-                </CardContent>
-              </Card>
-            </div>
-          </TabsContent>
-        </Tabs>
+                <div className="grid w-full  xl:grid-cols-2 ">
+                  <div className="flex flex-col xl:col-span-1 col-span-1 ml-5 ">
+                    <Card>
+                      <CardHeader className="flex items-start gap-4">
+                        <CardTitle className="text-base">
+                          Approve Items
+                        </CardTitle>
+                      </CardHeader>
+                      <CardContent className="p-0">
+                        <ScrollArea className="h-[300px] w-full rounded-md border p-4">
+                          <ul className="divide-y">
+                            <li className="flex items-center justify-between p-4 hover:bg-gray-100">
+                              <Link href="suggestion/123">
+                                <div className="flex items-center gap-4">
+                                  <img
+                                    alt="Avatar"
+                                    className="rounded-full"
+                                    height="40"
+                                    src="https://github.com/shadcn.png"
+                                    style={{
+                                      aspectRatio: "40/40",
+                                      objectFit: "cover",
+                                    }}
+                                    width="40"
+                                  />
+                                  <div className="grid gap-1.5">
+                                    <h3 className="font-semibold">Charlie</h3>
+                                    <p className="text-sm text-gray-500 dark:text-gray-400">
+                                      This is regarding my subscription.
+                                    </p>
+                                  </div>
+                                </div>
+                              </Link>
+                            </li>
+                            <li className="flex items-center justify-between p-4 hover:bg-gray-100">
+                              <Link href="suggestion/123">
+                                <div className="flex items-center gap-4">
+                                  <img
+                                    alt="Avatar"
+                                    className="rounded-full"
+                                    height="40"
+                                    src="https://github.com/shadcn.png"
+                                    style={{
+                                      aspectRatio: "40/40",
+                                      objectFit: "cover",
+                                    }}
+                                    width="40"
+                                  />
+                                  <div className="grid gap-1.5">
+                                    <h3 className="font-semibold">Charlie</h3>
+                                    <p className="text-sm text-gray-500 dark:text-gray-400">
+                                      This is regarding my subscription.
+                                    </p>
+                                  </div>
+                                </div>
+                              </Link>
+                            </li>
+                            <li className="flex items-center justify-between p-4 hover:bg-gray-100">
+                              <Link href="suggestion/123">
+                                <div className="flex items-center gap-4">
+                                  <img
+                                    alt="Avatar"
+                                    className="rounded-full"
+                                    height="40"
+                                    src="https://github.com/shadcn.png"
+                                    style={{
+                                      aspectRatio: "40/40",
+                                      objectFit: "cover",
+                                    }}
+                                    width="40"
+                                  />
+                                  <div className="grid gap-1.5">
+                                    <h3 className="font-semibold">Charlie</h3>
+                                    <p className="text-sm text-gray-500 dark:text-gray-400">
+                                      This is regarding my subscription.
+                                    </p>
+                                  </div>
+                                </div>
+                              </Link>
+                            </li>
+                            <li className="flex items-center justify-between p-4 hover:bg-gray-100">
+                              <Link href="suggestion/123">
+                                <div className="flex items-center gap-4">
+                                  <img
+                                    alt="Avatar"
+                                    className="rounded-full"
+                                    height="40"
+                                    src="https://github.com/shadcn.png"
+                                    style={{
+                                      aspectRatio: "40/40",
+                                      objectFit: "cover",
+                                    }}
+                                    width="40"
+                                  />
+                                  <div className="grid gap-1.5">
+                                    <h3 className="font-semibold">Charlie</h3>
+                                    <p className="text-sm text-gray-500 dark:text-gray-400">
+                                      This is regarding my subscription.
+                                    </p>
+                                  </div>
+                                </div>
+                              </Link>
+                            </li>
+                          </ul>
+                        </ScrollArea>
+                      </CardContent>
+                    </Card>
+                  </div>
+                  <div className="flex flex-col xl:col-span-1 col-span-1 ml-5 ">
+                    <Card>
+                      <CardHeader className="flex items-start gap-4">
+                        <CardTitle className="text-base">
+                          Suggestion Items
+                        </CardTitle>
+                      </CardHeader>
+                      <CardContent className="p-0">
+                        <ScrollArea className="h-[300px] w-full rounded-md border p-4">
+                          <SuggestTable />
+                        </ScrollArea>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </div>
+              </TabsContent>
+            </Tabs>
+          </div>
+        </ScrollArea>
       </div>
-    </ScrollArea>
-	</div>
-	</div>
-
+    </div>
   );
 }
---
+++ components/suggestTable.tsx
@@ -0,0 +1,117 @@
+"use client";
+
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
+
+interface SuggestItems {
+  image: string;
+  name: string;
+  description: string;
+  id: string;
+  sugessId: string;
+}
+
+export default function SuggestTable() {
+  const [suggestItems, setSuggestItems] = useState<SuggestItems[]>([]);
+
+
+  useEffect(() => {
+    let gocount =1;
+    if(gocount == 1){
+        fetch("/api/suggest/get")
+        .then((response) => response.json())
+        .then((data) => {
+  
+          const lostItemIds = data.flattenedArray;
+          lostItemIds.map((item: any) => {
+            // renderTable(item.foundItemId,item.id);
+            fetch(`/api/found/${item.foundItemId}`)
+            .then((response) => response.json())
+            .then((data) => {
+      
+              const { id, title, description, images } = data;
+              const suggestItem = {
+                image: images.toString(),
+                name: title,
+                description: description,
+                id: id,
+                sugessId:item.id
+              };
+      
+              setSuggestItems((prevItems) => [...prevItems, suggestItem]);
+            
+            })
+            .catch((error) => {
+              console.error("Error fetching data:", error);
+            });
+          });
+        })
+        .catch((error) => {
+          console.error("Error fetching user data:", error);
+        });
+      }
+      gocount++;
+  }, []);
+
+  return (
+    <ul className="divide-y">
+      {suggestItems.length === 0 ? (
+        <section className="flex items-start  py-6">
+          <div className="container flex flex-col items-center px-4 space-y-4">
+            <div className="flex flex-col items-center space-y-2 text-center">
+              <p className="text-lg font-medium text-gray-500 dark:text-gray-400">
+                No have suggestion item
+              </p>
+            </div>
+
+            <img
+              alt="GIF"
+              className="aspect-square"
+              height="150"
+              src="https://i.postimg.cc/B6yVSWfW/searching-find.gif"
+              width="150"
+            />
+          </div>
+        </section>
+      ) : (
+        suggestItems.map((item) => (
+          <li
+            className="flex items-center justify-between p-4 hover:bg-gray-100"
+            key={item.id}
+          >
+            {/* <Link href={`suggestion/${item.id}`}> */}
+            <Link
+              href={{
+                pathname: "/suggestion",
+                query: {
+                  id: item.id,
+                  sugessId:item.sugessId,
+                },
+              }}
+            >
+              <div className="flex items-center gap-4">
+                <img
+                  alt="Avatar"
+                  className="rounded-full"
+                  height="40"
+                  src="https://github.com/shadcn.png"
+                  style={{
+                    aspectRatio: "40/40",
+                    objectFit: "cover",
+                  }}
+                  width="40"
+                />
+                <div className="grid gap-1.5">
+                  <h3 className="font-semibold">{item.name}</h3>
+                  <p className="text-sm text-gray-500 dark:text-gray-400">
+                    {item.description}
+                  </p>
+                </div>
+              </div>
+            </Link>
+          </li>
+        ))
+      )}
+    </ul>
+  );
+}
