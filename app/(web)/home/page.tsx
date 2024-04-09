b5e325c6736fc9a0e6c5fefebcf3114cc0c4c7e6
Randika Dilshan <rdilshan077788@gmail.com>
2024-04-09 16:06:48 +0530

    nav bar add

--- app/(web)/home/page.tsx
+++ app/(web)/home/page.tsx
@@ -30,55 +30,7 @@ export default function HomePage() {
 
   return (
 	<div>
-    <nav className="bg-white w-full border-b md:border-0">
-      <div className="items-center px-4 max-w-screen-xl mx-auto md:flex md:px-8">
-        <div className="flex items-center justify-between py-3 md:py-5 md:block">
-          <Link href="/">
-            <h1 className="text-3xl font-bold text-purple-600">Logo</h1>
-          </Link>
-          <div className="md:hidden">
-            <button
-              className="text-gray-700 outline-none p-2 rounded-md focus:border-gray-400 focus:border"
-              onClick={() => setState(!state)}
-            >
-              <Menu />
-            </button>
-          </div>
-        </div>
-        <div
-          className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
-            state ? "block" : "hidden"
-          }`}
-        >
-          <ul className="items-center justify-end space-y-8 md:flex md:space-x-6 md:space-y-0">
-            <li className="text-gray-600 hover:text-indigo-600">
-              <Link href="/new-lost">add post</Link>
-            </li>
-            <li className="text-gray-600 hover:text-indigo-600">
-              <Link href="/new-lost">Your post</Link>
-            </li>
 
-            <DropdownMenu>
-              <DropdownMenuTrigger>
-  
-                <Avatar>
-                  <AvatarImage src="https://github.com/shadcn.png" />
-                  <AvatarFallback>CN</AvatarFallback>
-                </Avatar>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>Profile</DropdownMenuItem>
-                <DropdownMenuItem>Billing</DropdownMenuItem>
-                <DropdownMenuItem>Team</DropdownMenuItem>
-                <DropdownMenuItem>Subscription</DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </ul>
-        </div>
-      </div>
-    </nav>
 
 	<div>
 	<ScrollArea className="h-full">
