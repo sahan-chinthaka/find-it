f65c2462bf30ab6e3523df9f772d5d1c35f9dd52
sahan-chinthaka <sahanchinthaka04@gmail.com>
2024-04-10 09:57:19 +0530

    Modify the design of lost and found view

--- app/(web)/found/page.tsx
+++ app/(web)/found/page.tsx
@@ -1,4 +1,6 @@
+import { Button } from "@/components/ui/button";
 import { cookiesToString } from "@/lib/utils";
+import { PlusIcon } from "lucide-react";
 import { cookies } from "next/headers";
 import Link from "next/link";
 
@@ -12,16 +14,38 @@ async function FoundPage() {
 	const data = await founds.json();
 
 	return (
-		<div>
-			<Link className="text-blue-500" href={"/new-found"}>Add New Found</Link>
-			{data.map((a: any) => (
-				<div key={a.id} className="p-2 m-2 bg-slate-200">
-					<p>{a.title}</p>
-					<p>{a.description}</p>
-					<p><Link className="text-blue-500" href={"/found/" + a.id}>Details</Link></p>
-				</div>
-			))}
-		</div>
+		<>
+			<div className="flex justify-start p-4 mx-7">
+				<Link href={"/new-found"}>
+					<Button size="lg" variant="outline" className="bg-green-200">
+						<PlusIcon className="mr-2 h-4 w-4" />
+						New Found
+					</Button>
+				</Link>
+			</div>
+			<section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-4 md:p-6 mx-7">
+				{data.map((a: any) => (
+					<Link key={a.id} href={"/found/" + a.id}>
+						<div className="border-dashed border border-gray-400 rounded-lg dark:border-gray-800 dark:hover:border-gray-50">
+							<img
+								alt="Product 1"
+								className="object-cover w-full h-60"
+								height={300}
+								src={a.images[0]}
+								style={{
+									aspectRatio: "400/300",
+									objectFit: "cover",
+								}}
+								width={400}
+							/>
+							<div className="bg-white p-4 dark:bg-gray-950">
+								<h3 className="font-semibold text-lg md:text-xl">{a.title}</h3>
+							</div>
+						</div>
+					</Link>
+				))}
+			</section>
+		</>
 	);
 }
 
